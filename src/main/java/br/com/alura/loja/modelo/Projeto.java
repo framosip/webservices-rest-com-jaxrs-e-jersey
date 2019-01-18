8d7f32bd915c078d37cabc57615cd1e463e365fc
Fernando Ramos <framosip@gmail.com>
2019-01-18 14:24:00 -0200

    Projeto Resource, busca pelo primeiro projeto acessando /projetos e test de retorno ao acessar /projetos

---
+++ src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -0,0 +1,28 @@
+package br.com.alura.loja.modelo;
+
+import com.thoughtworks.xstream.XStream;
+
+public class Projeto {
+
+	private long id;
+	private String nome;
+	private int anoDeInicio;
+
+	public Projeto(long id, String nome, int anoDeInicio) {
+		this.id = id;
+		this.nome = nome;
+		this.anoDeInicio = anoDeInicio;
+	}
+	
+	public Projeto() {
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String toXML() {
+		return new XStream().toXML(this);
+	}
+
+}
