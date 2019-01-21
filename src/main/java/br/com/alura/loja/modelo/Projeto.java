8fc047c92b6131773b013a3a10cc15c15ef61a0a
Fernando Ramos <framosip@gmail.com>
2019-01-21 13:23:41 -0200

    Testes na classe Projeto

--- src/main/java/br/com/alura/loja/modelo/Projeto.java
+++ src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -20,6 +20,10 @@ public class Projeto {
 	public void setId(long id) {
 		this.id = id;
 	}
+	
+	public String getNome() {
+		return nome;
+	}
 
 	public String toXML() {
 		return new XStream().toXML(this);
