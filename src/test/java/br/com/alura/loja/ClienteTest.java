6b9853d085d842df4c255ed0705d1092448dfea3
Fernando Ramos <framosip@gmail.com>
2019-01-21 14:13:22 -0200

    Usando POST para adicionar recursos. Criado teste para testar a criação de um novo carrinho

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -4,7 +4,10 @@ import java.net.URISyntaxException;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -14,6 +17,7 @@ import org.junit.Test;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 import junit.framework.Assert;
 
 public class ClienteTest {
@@ -39,6 +43,27 @@ public class ClienteTest {
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+	
+	@Test
+	public void testaAdicionarUmCarrinho() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Sao Paulo");
+        
+        String xml = carrinho.toXML();
+        
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+        
+        Response response = target.path("/carrinhos").request().post(entity);
+        
+        Assert.assertEquals("<status>Sucesso</status>", response.readEntity(String.class));
+        
+        
 	}
 	
 }
--- src/test/java/br/com/alura/loja/ProjetoTest.java
+++ src/test/java/br/com/alura/loja/ProjetoTest.java
@@ -11,7 +11,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Projeto;
 import junit.framework.Assert;
@@ -36,7 +36,7 @@ public class ProjetoTest {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
 		String conteudo = target.path("/projetos/1").request().get(String.class);
-		Projeto projeto = new Gson().fromJson(conteudo, Projeto.class);
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
 		
 		Assert.assertEquals("Minha loja", projeto.getNome());
 	}
